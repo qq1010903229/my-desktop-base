dfea82b1ad36c272a940512b1aca27d36f88a6dd
qq1010903229 <1010903229@qq.com>
2019-09-15 17:49:13 +0800

    Push.cmd: Add/Modify Init.cmd

--- Init.cmd
+++ Init.cmd
@@ -4,5 +4,5 @@ echo echo Initing... >>temp\Init.cmd
 echo git init >>temp\Init.cmd
 echo git remote add origin git@github.com:qq1010903229/my-desktop.git >>temp\Init.cmd
 echo git pull origin master >>temp\Init.cmd
-echo start /min "" ping 127.0.0.1 -n 2 && rd /s /q temp >>temp\Init.cmd
+echo start /min "" ping 127.0.0.1 -n 2 ^&^& rd /s /q temp >>temp\Init.cmd
 start "Init My Desktop" temp\Init.cmd
