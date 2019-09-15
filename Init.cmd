bfdf720a132dd19e1b267ac070dff8438fe50a13
qq1010903229 <1010903229@qq.com>
2019-09-15 17:51:09 +0800

    Push.cmd: Add/Modify Init.cmd

--- Init.cmd
+++ Init.cmd
@@ -1,5 +1,7 @@
 @echo off
 md temp
+echo @echo off >>temp\Init.cmd
+echo del Init.cmd >>temp\Init.cmd
 echo echo Initing... >>temp\Init.cmd
 echo git init >>temp\Init.cmd
 echo git remote add origin git@github.com:qq1010903229/my-desktop.git >>temp\Init.cmd
